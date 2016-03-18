e4c145356df942c0c62cff00adbe72cea73cfa24
Lukas Gemar <Lukas Gemar>
2016-03-18 15:40:18 -0400

    I think the MEKF is working pretty well. Reinitialized with accel and magn in their regular units

--- Analysis/Analysis-3-17/MekfSandbox.m
+++ Analysis/Analysis-3-17/MekfSandbox.m
@@ -4,7 +4,7 @@
 DataFolder = (['C:\Users\Lukas Gemar\thesis\Analysis\Analysis-3-17\', 'Data\Sensor\']); 
 AllTestNames = {'roll1', 'pitch1', 'yaw1'}; 
 
-TestName = AllTestNames{3}; 
+TestName = AllTestNames{1}; 
 
 SensorFile = [TestName, '.csv']; 
 SensorData = csvread([DataFolder, SensorFile]); SensorData = SensorData(2:end, :); 
@@ -14,17 +14,20 @@ N = size(SensorData,1); % Number of samples
 dt = mean(diff(tSensor)); % Simulation time step
 t = tSensor - min(tSensor); 
 
-% Reference vectors
-g = [0; 0; 1]; % world coordinates
-b = [cos(-pi/2-0.05)*1; sin(-pi/2-0.05)*1; 0]; % world coordinates
-
 % Simulation variables
 qref = SensorData(:,2:5); % quaternion reference array
 qestT = zeros(N,4); % Triad estimates
 qestK = zeros(N,4); % Kalman filter estimates
-z = [SensorData(:,6:8)./repmat(VectorNorm(SensorData(:,6:8)),1,3)...
-     SensorData(:,12:14)./repmat(VectorNorm(SensorData(:,12:14)),1,3)]; % sensor measurements
+% z = [SensorData(:,6:8)./repmat(VectorNorm(SensorData(:,6:8)),1,3)...
+%      SensorData(:,12:14)./repmat(VectorNorm(SensorData(:,12:14)),1,3)]; % sensor measurements
+z = [SensorData(:,6:8),...
+     SensorData(:,12:14)]; % sensor measurements
 omega = SensorData(:,9:11); % gyro measurements
+
+% Reference vectors
+g = [0; 0; 9.81]; % world coordinates
+% b = [cos(-pi/2-0.05)*1; sin(-pi/2-0.05)*1; 0]; % world coordinates
+b = z(1,4:6)'; 
 
 figure(1)
 subplot(2,2,1)
@@ -38,12 +41,11 @@ legend('X', 'Y', 'Z')
 subplot(2,2,3)
 plot( t,z(:,1:3) )
 title('Accelerometer')
-ylim([-1.1 1.1])
+ylim([-10 10])
 legend('X', 'Y', 'Z')
 subplot(2,2,4)
 plot( t, z(:,4:6) )
 title('B Field')
-ylim([-1.1 1.1])
 legend('X', 'Y', 'Z')
 
 
@@ -102,9 +104,9 @@ mb = z(1,4:6)'; % measurement of b vector in body
 qe = triadFun(g,mg,b,mb,eye(3,3)); % column-major quaternion 
 
 % Initalize the covariance matrix, process noise, and measurement noise
-Sp = [0.006 0.007 0.009]; Pe = diag(Sp); 
-Sq = [3e-5 3e-5 3e-5]'; Q = diag(Sq); 
-Sr = [0.008*ones(1,3) 0.008 0.008 0.008]'; R = diag(Sr); 
+Sp = [0.006 0.007 0.08]; Pe = diag(Sp); 
+Sq = [2.8e-6 2.8e-6 2.8e-6]'; Q = diag(Sq); 
+Sr = [1.1e-6*ones(1,3) 17*ones(1,3)]'; R = diag(Sr); 
 
 for k = 2:N
 
