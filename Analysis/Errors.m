7ce49b7154a332340a2abf70b57f27452f261345
Lukas Gemar <Lukas Gemar>
2016-03-26 00:53:31 -0400

    Added final report to repository

--- Analysis/Errors.m
+++ Analysis/Errors.m
@@ -1,11 +1,11 @@
 %% 
 
-T = [1 0 0; 0 0 -1; 0 1 0]; 
-pCpr = [0 10 0]'; 
-pC = T * pCpr; 
-pCprX = CrossMat(pCpr); 
-pCX = CrossMat(pC); 
-Pdthetapr = diag([deg2rad(10).^2 0 deg2rad(10).^2]); 
+T = [1 0 0; 0 0 -1; 0 1 0]; % rotation matrix (A')
+pCpr = [0 10 0]'; % body coordinates
+pC = T * pCpr; % world coordinates
+pCprX = CrossMat(pCpr); % direction of variance in body coordinates
+pCX = CrossMat(pC); % direction of variance in world coordinates
+Pdthetapr = diag([deg2rad(10).^2 0 deg2rad(10).^2]); % variance of angles in body coordinates
 
 Pdtheta1 = pCX * T * Pdthetapr * T' * pCX'
 
